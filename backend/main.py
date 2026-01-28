0482dede5d2ba7ffe870882e40fb9e2c02e984a0
Anugrah Awwal <muhamadanugrah36@gmail.com>
2026-01-28 11:09:10 +0700

    fix(cors): fallback to allow_origins=* for maximum compatibility

--- backend/main.py
+++ backend/main.py
@@ -28,8 +28,8 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 # Flexible CORS for Development
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex="http://(localhost|127\.0\.0\.1)(:[0-9]+)?",
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
